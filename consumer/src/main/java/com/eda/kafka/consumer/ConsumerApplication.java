f75ad6996a94d163ea2c74ce34cb924472bf9c9d
Aaron Majambo <aaronmajb@gmail.com>
2025-01-21 14:50:33 +0400

    Setting up the consumer application

--- consumer/src/main/java/com/eda/kafka/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/eda/kafka/consumer/ConsumerApplication.java
@@ -1,4 +1,12 @@
 package com.eda.kafka.consumer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 }
--- consumer/src/main/java/com/eda/kafka/consumer/MessageConsumer.java
+++ consumer/src/main/java/com/eda/kafka/consumer/MessageConsumer.java
@@ -1,4 +1,13 @@
 package com.eda.kafka.consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageConsumer {
+
+    @KafkaListener(topics = "messageTopic", groupId = "message")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
 }
